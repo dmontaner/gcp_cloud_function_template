bfaab1772d45539b7c39e08b67339506ad36dbd2
dmontaner <david.montaner@gmail.com>
2024-01-31 18:36:20 +0000

    notes

--- {{cookiecutter.directory_name}}/deploy.sh
+++ {{cookiecutter.directory_name}}/deploy.sh
@@ -5,6 +5,14 @@
 
 # The project needs to be authorized to create and use cloud functions.
 # I generally create an example function in the web console and it will ask me about all permits.
+#
+# But you can also do it using the command line. See:
+# https://getbetterdevops.io/google-cloud-functions-with-terraform/#enable-needed-google-services
+#
+# gcloud services enable cloudfunctions.googleapis.com
+# gcloud services enable run.googleapis.com
+# gcloud services enable artifactregistry.googleapis.com
+# gcloud services enable cloudbuild.googleapis.com
 
 gcloud functions deploy {{ cookiecutter.function_name }} \
        --memory=128Mi \
